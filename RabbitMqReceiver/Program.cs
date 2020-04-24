71de7f779d6750a52a4c75ba5233b1170dbdb539
Stefaan Haeck <stefaan.haeck@student.ehb.be>
2020-04-24 17:05:24 +0200

    Heartbeat works

--- RabbitMqReceiver/Program.cs
+++ RabbitMqReceiver/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Text;
+using System.Threading;
+using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Schema;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -10,11 +14,88 @@ namespace RabbitMqReceiver
     {
         static void Main(string[] args)
         {
-            ReceiverRabbitMQUsers();
+            //ReceiverRabbitMQUsers();
+            Heartbeat();
 
 
 
         }
+
+        private static void Heartbeat() {
+            TimerCallback callback = HeartBeatCall;
+            Timer timer = new Timer(HeartBeatCall, "test", 500, 500);
+            Console.WriteLine("Press any key to exit the sample");
+            Console.ReadLine();
+
+        }
+
+        private static void HeartBeatCall(object objectInfo)
+        {
+            var datetime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff");
+
+            string xml;
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    writer.WriteStartElement("heartbeat");
+                    writer.WriteElementString("application_name", "frontend");
+                    writer.WriteElementString("timestamp", datetime);
+                    writer.WriteEndElement();
+                    writer.Flush();
+                    xml = sww.ToString();
+                }
+            }
+
+            //XML validation with XSD
+            string xsdData =
+                @"<?xml version='1.0'?> 
+                    <xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'> 
+                     <xs:element name='heartbeat'> 
+                      <xs:complexType> 
+                       <xs:sequence> 
+                        <xs:element name='application_name' type='xs:string'/> 
+                        <xs:element name='timestamp' type='xs:string'/> 
+                       </xs:sequence> 
+                      </xs:complexType> 
+                     </xs:element> 
+                    </xs:schema>";
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", XmlReader.Create(new StringReader(xsdData)));
+
+            var xDoc = XDocument.Parse(xml);
+            bool errors = false;
+            xDoc.Validate(schemas, (o, e) =>
+            {
+                errors = true;
+            });
+
+            if (!errors) {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "192.168.1.2",
+                    Port = 5672,
+                    UserName = "frontend_user",
+                    Password = "frontend_pwd"
+
+                };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    var addUserBody = Encoding.UTF8.GetBytes(xml);                    
+                    channel.BasicPublish(exchange: "heartbeats.exchange",
+                                     routingKey: "",                                     
+                                     body: addUserBody
+                                     );
+                }
+            }
+
+            
+
+            
+
+        }
+
         private static void ReceiverRabbitMQUsers()
         {
             var factory = new ConnectionFactory()
